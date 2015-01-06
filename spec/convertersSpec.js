cb50554819dc9605e62c56629875d8cab55823c6
Magnus Siverbrant <siven@paddelkraft.se>
2015-01-06 17:38:09 +0100

    Rewritten with promises
    have rewritten the approveIt so it uses promises
    rewrite also enables asynchronous tests
    the style of the tests now looks more like regular approval tests with
    an approvals.verify(output) at the end

--- spec/convertersSpec.js
+++ spec/convertersSpec.js
@@ -4,22 +4,36 @@ describe("Converters", function() {
     
   });
 
-  //approval test
-  approveIt("should approve object", function() {
+  approveIt("should approve object", function(approvals) {
     var object = {};
     object.id = 100;
     object.name = "testObject";
     object.href = "http://url.com/test.txt";
-    return object;
+    approvals.verify(object);
   });
 
-  //approval test
-  approveIt("should not be approved yet", function() {
+  approveIt("should not be same as approved", function(approvals) {
     var object = {};
     object.id = 100;
     object.name = "testObject";
     object.href = "http://url.com/test.txt";
-    return object;
+    approvals.verify(object);
+  });
+
+  approveIt("should have no approved output yet", function(approvals) {
+    var object = {};
+    object.id = 100;
+    object.name = "testObject";
+    object.href = "http://url.com/test.txt";
+    approvals.verify(object);
+  });
+
+  it("failing regular test", function() {
+    var object = {};
+    object.id = 100;
+    object.name = "testObject";
+    object.href = "http://url.com/test.txt";
+    expect(object).toBe({"id":101,"name":"testObject","href":"http://url.com/test.txt"});
   });
 
   //regular test
